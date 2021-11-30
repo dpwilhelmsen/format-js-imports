d8836fd16bf987b0390bde9ec0ad67addc47846b
GitHub Action <action@github.com>
2021-11-30 01:00:30 +0000

    Template cleanup

--- src/main/kotlin/com/github/dpwilhelmsen/formatjsimports/MyBundle.kt
+++ src/main/kotlin/com/github/dpwilhelmsen/formatjsimports/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.dpwilhelmsen.formatjsimports
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
---
+++ src/main/kotlin/com/github/dpwilhelmsen/formatjsimports/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.dpwilhelmsen.formatjsimports.services
+
+import com.github.dpwilhelmsen.formatjsimports.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
